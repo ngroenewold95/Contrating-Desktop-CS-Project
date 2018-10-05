8c5feaf80f343942ae552842f764e52cb4a4219d
alexk <kostiuka4@mymacewan.ca>
2018-10-05 09:35:20 -0600

    SQL tables

---
+++ ContractingDesktop/ContractingDesktop/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ContractingDesktop
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.password = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-6, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 521);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Controls.Add(this.login);
+            this.panel2.Controls.Add(this.username);
+            this.panel2.Controls.Add(this.password);
+            this.panel2.Location = new System.Drawing.Point(182, 220);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(426, 231);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.password.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.password.Location = new System.Drawing.Point(113, 59);
+            this.password.Name = "textBox1";
+            this.password.Size = new System.Drawing.Size(193, 22);
+            this.password.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.username.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.username.Location = new System.Drawing.Point(113, 104);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(193, 22);
+            this.username.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.login.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.login.Location = new System.Drawing.Point(153, 152);
+            this.login.Name = "loginbutton";
+            this.login.Size = new System.Drawing.Size(104, 32);
+            this.login.TabIndex = 2;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel3.Location = new System.Drawing.Point(182, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(426, 126);
+            this.panel3.TabIndex = 2;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 516);
+            this.Controls.Add(this.panel1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox username;
+        
+    }
+}
--- ContractingDesktop/ContractingDesktop/Login.cs
+++ ContractingDesktop/ContractingDesktop/Login.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace ContractingDesktop
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }

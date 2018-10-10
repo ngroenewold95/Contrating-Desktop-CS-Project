3c316764968fedfd74dfa56736047280672adae7
alexk <kostiuka4@mymacewan.ca>
2018-10-10 08:32:59 -0600

    Add Contractor, Client and Contract to Dataset by TextField

---
+++ ContractingDesktop/ContractingDesktop/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ContractingDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmailText = new System.Windows.Forms.TextBox();
+            this.LastText = new System.Windows.Forms.TextBox();
+            this.FirstText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ClientText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EmailText
+            // 
+            this.EmailText.Location = new System.Drawing.Point(206, 216);
+            this.EmailText.Name = "EmailText";
+            this.EmailText.Size = new System.Drawing.Size(100, 22);
+            this.EmailText.TabIndex = 3;
+            // 
+            // LastText
+            // 
+            this.LastText.AcceptsReturn = true;
+            this.LastText.Location = new System.Drawing.Point(206, 172);
+            this.LastText.Name = "LastText";
+            this.LastText.Size = new System.Drawing.Size(100, 22);
+            this.LastText.TabIndex = 2;
+            // 
+            // FirstText
+            // 
+            this.FirstText.Location = new System.Drawing.Point(206, 132);
+            this.FirstText.Name = "FirstText";
+            this.FirstText.Size = new System.Drawing.Size(100, 22);
+            this.FirstText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "First Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ClientText
+            // 
+            this.ClientText.Location = new System.Drawing.Point(206, 257);
+            this.ClientText.Name = "ClientText";
+            this.ClientText.Size = new System.Drawing.Size(100, 22);
+            this.ClientText.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Client Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(122, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ClientText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FirstText);
+            this.Controls.Add(this.LastText);
+            this.Controls.Add(this.EmailText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox EmailText;
+        private System.Windows.Forms.TextBox LastText;
+        private System.Windows.Forms.TextBox FirstText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ClientText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
